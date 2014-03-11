9277bc49d94b0ba5c25f9fe4df5411ba139d4c5f
EC <mr.oren.mor@gmail.com>
2014-03-11 12:53:58 +0200

    addinfection view conroller

--- EpidemicControl/addInfectionViewController.m
+++ EpidemicControl/addInfectionViewController.m
@@ -27,6 +27,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +35,33 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+   return 5;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"disease"];
+    return cell;
+
+}
+
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 30.0;
+}
+
+
+
+
+
+
 
 @end
